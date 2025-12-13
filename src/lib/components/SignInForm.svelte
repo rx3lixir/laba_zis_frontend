bd32f866060a2ea2b3af528546ff830d6f790031
rx3lixir <i027356@inbox.ru>
2025-12-14 01:38:52 +0700

    atomic websockets

--- src/lib/components/SignInForm.svelte
+++ src/lib/components/SignInForm.svelte
@@ -3,6 +3,9 @@
 
   import { enhance } from "$app/forms";
   import { goto } from "$app/navigation";
+  import { Input } from "$lib/components/ui/input";
+  import { Button } from "$lib/components/ui/button";
+  import { Label } from "$lib/components/ui/label";
 
   let email = $state('');
   let password = $state('');
@@ -83,19 +86,15 @@
 
     <!-- Email block -->
     <div class="space-y-2">
-      <label for="email" class="block text-sm font-medium text-stone-700">
-        Email
-      </label>
+      <Label for="email">Email</Label>
 
-      <input 
-        id="email"
+      <Input 
+        id="email" 
         name="email"
         type="email"
         autocomplete="email"
         required
         bind:value={email}
-        class="block w-full rounded-md border border-stone-300 px-3 py-2 shadow-sm focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-400"
-        class:border-red-400={errors.email}
       />
 
       {#if errors.email}
@@ -105,20 +104,16 @@
 
     <!-- Password block -->
     <div class="space-y-2">
-      <label for="password" class="block text-sm font-medium text-stone-700">
-        Password
-      </label>
+      <Label for="email">Password</Label>
 
-      <input
-        id="password"
+      <Input 
+        id="password" 
         name="password"
         type="password"
         autocomplete="current-password"
         required
         bind:value={password}
-        class="block w-full rounded-md border border-stone-300 px-3 py-2 shadow-sm focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-400"
-        class:border-red-500={errors.password}
-      />
+      ></Input>
 
       {#if errors.password}
         <p class="text-sm text-red-600">{errors.password}</p>
@@ -126,14 +121,12 @@
     </div>
 
     <!-- Submit button -->
-    <button
+    <Button 
       type="submit"
       disabled={isSubmitting}
-      class="w-full rounded-md bg-blue-500 px-4 py-2 text-white font-medium hover:bg-blue-400 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
     >
       {isSubmitting ? 'Signing in...' : 'Sign in'}
-    </button>
-
+    </Button>
     <!-- Signup redirect button -->
     <p class="text-center text-sm text-stone-800">
       Not registered yet?
